2278d803b12c2943878f024e1bf3d735d9e767a3
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-07-15 12:15:50 +0200

    inner html

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+console.log("Hallo Heat")
+
+
+document.getElementById('gallery').innerHTML = '<figure><img src="https://images.unsplash.com/photo-1657861242319-b08f8660dbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt="berge" style="width:100%"><figcaption>pic1</figcaption></figure>'
+
+document.getElementById('gallery').innerHTML += '<figure><img src="https://images.unsplash.com/photo-1657861242319-b08f8660dbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt="berge" style="width:100%"><figcaption>pic2</figcaption></figure>'
+
+document.getElementById('gallery').innerHTML += '<figure><img src="https://images.unsplash.com/photo-1657861242319-b08f8660dbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt="berge" style="width:100%"><figcaption>pic3</figcaption></figure>'
+
+
+
+
+//document.getElementById('gallery').innerHTML += '<figure>< img src = "https://images.unsplash.com/photo-1657861242319-b08f8660dbb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60" alt = "berge" style="width:100%"><figcaption>pic3</figcaption></figure > '
+
